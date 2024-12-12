3d031aa72c537862271369c0291f718c7c69cebe
Namit Malasi <namitmalasi@gmail.com>
2024-12-12 19:31:32 +0530

    adding routes for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+import JobListing from "./pages/job-listing";
+import JobPage from "./pages/job";
+import PostJob from "./pages/post-job";
+import SavedJobs from "./pages/saved-jobs";
+import MyJobs from "./pages/my-jobs";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <Onboarding />,
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/job/:id",
+        element: <JobPage />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/saved-jobs",
+        element: <SavedJobs />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/post-job.jsx
@@ -0,0 +1,8 @@
+
+const PostJob = () => {
+  return (
+    <div>PostJob</div>
+  )
+}
+
+export default PostJob
