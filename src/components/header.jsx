0aa091976f72ab3c561f56d83767fa1aee0aa6ef
Namit Malasi <namitmalasi@gmail.com>
2024-12-14 19:18:05 +0530

    landing page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,13 +14,13 @@ const Header = () => {
         <Link>
           <h1 className="text-2xl">JobSphere</h1>
         </Link>
-        {/* <Button variant="outline">Login</Button> */}
-        <SignedOut>
+        <Button variant="outline">Login</Button>
+        {/* <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
           <UserButton />
-        </SignedIn>
+        </SignedIn> */}
       </nav>
     </>
   );
